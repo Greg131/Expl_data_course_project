4f8dfb13201c131500324ab73664841bff416038
Greg131 <gregoire_argenton@yahoo.fr>
2015-08-22 13:57:54 +0200

    draft 3

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Expl_data_course_project
 Course Project
+
+Files available :
+run_analysis.R
+CodeBook.md
--- run_analysis.R
+++ run_analysis.R
@@ -1,21 +1,29 @@
-setwd("~/Dropbox/Datascience/Universite_Johns-Hopkins/Obtenir_et_trier_les_donnees/Repo")
+setwd("~/Dropbox/Datascience/Universite_Johns-Hopkins/Obtenir_et_trier_les_donnees/Repo/data/UCI HAR Dataset")
 
 #  1 - Merges the training and the test sets to create one data set.
 
-Xtest <- read.table("./data/UCI HAR Dataset/test/X_test.txt", sep="", header = FALSE)
-Ytest <- read.table("./data/UCI HAR Dataset/test/Y_test.txt", sep="", header = FALSE)
+Xtest <- read.table("./test/X_test.txt", sep="", header = FALSE)
+Ytest <- read.table("./test/Y_test.txt", sep="", header = FALSE)
+Subjecttest <- read.table("./test/subject_test.txt", sep="", header = FALSE)
 
-Xtrain <- read.table("./data/UCI HAR Dataset/train/X_train.txt", sep="", header = FALSE)
-Ytrain <- read.table("./data/UCI HAR Dataset/train/Y_train.txt", sep="", header = FALSE)
+Xtrain <- read.table("./train/X_train.txt", sep="", header = FALSE)
+Ytrain <- read.table("./train/Y_train.txt", sep="", header = FALSE)
+Subjecttrain <- read.table("./train/subject_train.txt", sep="", header = FALSE)
 
 Xmerged <- rbind(Xtest,Xtrain) 
 Ymerged <- rbind(Ytest,Ytrain) 
+Subjectmerged <- rbind(Subjecttest,Subjecttrain) 
+
+
 
 #--------------------------------------------------------------------------------------------
 # 4 - Appropriately labels the data set with descriptive variable names. 
 #--------------------------------------------------------------------------------------------
-features_names <- read.table("./data/UCI HAR Dataset/features.txt", sep="", header = FALSE)
+features_names <- read.table("./features.txt", sep="", header = FALSE)
 names(Xmerged) <- features_names[,2]
+names(Ymerged) <- "activity"
+names(Subjectmerged) <- "subject"
+
 
 #--------------------------------------------------------------------------------------------
 # 2 - Extracts only the measurements on the mean and standard deviation for each measurement. 
@@ -34,11 +42,18 @@ features_names[std,2]
 class(mean)
 XSubset <- Xmerged[,c(mean,std)]
 
+
 #--------------------------------------------------------------------------------------------
 # 3- Uses descriptive activity names to name the activities in the data set
 #--------------------------------------------------------------------------------------------
-activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt", sep="", header = FALSE)
+activity_labels <- read.table("./activity_labels.txt", sep="", header = FALSE)
 
+
+
+# Adding Subject and Y the the X data set
+
+DataSet <- cbind(Subjectmerged,XSubset,Ymerged)
+names(DataSet)
 
 
 # 5 - From the data set in step 4, creates a second, independent tidy data set 
